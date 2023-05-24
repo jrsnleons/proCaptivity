d925432327edf1d1af9f248fa180a0e23e1810a8
Joel T. Jayme <67952167+Noob11413@users.noreply.github.com>
2023-05-24 20:57:48 +0800

    Calculator Backend done

--- ProductivityApp/Views/CalculatorPage.xaml.cs
+++ ProductivityApp/Views/CalculatorPage.xaml.cs
@@ -6,84 +6,79 @@ using Xamarin.Forms.Xaml;
 namespace ProductivityApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CalculatorPage : ContentPage
+    public partial class CalculatorPage : ContentPage   
     {
-        private string currentNumber;
-        private string operation;
-        private double firstNumber;
-        private double secondNumber;
 
         public CalculatorPage()
         {
             InitializeComponent();
-            currentNumber = string.Empty;
         }
+
+            private string operation;
+            private decimal firstNumber;
+            private bool IsoperatorCliked;
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            if (sender is Button button)
+           var button = sender as Button;
+
+            if(BtnRes.Text == "0" || IsoperatorCliked)
             {
-                currentNumber += button.Text;
-                resultLabel.Text = currentNumber;
+                IsoperatorCliked = false;
+                BtnRes.Text = button.Text;
             }
+            else
+            {
+                BtnRes.Text += button.Text;
+            }
+
         }
 
         private void Clear_Clicked(object sender, EventArgs e)
         {
-            currentNumber = string.Empty;
-            firstNumber = 0;
-            secondNumber = 0;
-            operation = string.Empty;
-            resultLabel.Text = string.Empty;
+            BtnRes.Text = "0";
+        }
+
+       
+        private void Btn_Oper(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            IsoperatorCliked = true;
+            operation = button.Text;
+            firstNumber = Convert.ToDecimal(BtnRes.Text);
         }
 
         private void Equal_Clicked(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(currentNumber) && !string.IsNullOrEmpty(operation))
-            {
-                double secondNumber;
-                if (double.TryParse(currentNumber, out secondNumber))
-                {
-                    double result;
-                    switch (operation)
-                    {
-                        case "+":
-                            result = firstNumber + secondNumber;
-                            break;
-                        case "-":
-                            result = firstNumber - secondNumber;
-                            break;
-                        case "*":
-                            result = firstNumber * secondNumber;
-                            break;
-                        case "/":
-                            if (secondNumber != 0)
-                                result = firstNumber / secondNumber;
-                            else
-                            {
-                                resultLabel.Text = "Error";
-                                return;
-                            }
-                            break;
-                        default:
-                            resultLabel.Text = "Error";
-                            return;
-                    }
-
-                    resultLabel.Text = result.ToString();
-                    firstNumber = result;
-                    currentNumber = result.ToString();
-                    operation = string.Empty;
-                }
-                else
-                {
-                    resultLabel.Text = "Error";
-                }
-            }
+           decimal secondNumber = Convert.ToDecimal(BtnRes.Text);
+            decimal finalResult = Calculate(firstNumber,secondNumber);
+            BtnRes.Text = finalResult.ToString();
         }
 
+        public decimal Calculate(decimal firstNumber, decimal secondNumber)
+        {
+            decimal result = 0;
+            if (operation == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (operation == "-")
+            {
+                result = secondNumber - firstNumber;
 
+            }
+            else if (operation == "*")
+            {
+                result = (firstNumber * secondNumber);
 
+            }
+            else if (operation == "/")
+            {
 
+                result = firstNumber / secondNumber;
+            }
+            return result;
+        }
     }
+
 }
